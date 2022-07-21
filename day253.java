2de75cffb038ea450ad8f936121c5c758c11483a
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2022-07-21 22:23:30 +0200

    fixed day253

---
+++ day253.java
@@ -0,0 +1,14 @@
+public class day253 {
+    public static void main(String[] args){
+
+    }
+    void body(String s){
+        int length = s.length();
+        int count = 0;
+        int row = 0;
+        int col = 0;
+        while(count < length){
+            System.out.println();
+        }
+    }
+}
